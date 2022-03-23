f967acc142073e9c204942b6a8010652067afb1f
Abhinav Ramesh <64025335+AbhiByte@users.noreply.github.com>
2022-03-23 15:15:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,198 @@
     - Clean up main function, logic for gameOver and ensure robot works as expected
 */
 const int SQUARESIZE = 25;
+//todo
+//stop process if it has already placed smt
+//if there is 2 of one and one of the other it will say it should play but wont find a place to play see if that needs to be fixed
+//rest of logic
+
+//assuming 1 as x -1 as o and 0 as empty
+void hardMode(int gameBoard[3][3], int letter)
+{
+	//check if it can win
+	int sum = 0;
+	int count1 = 0;
+	int row = 0;
+	int col = 0;
+
+	//check rows
+	while((sum != 2*letter) && count1 < 3)
+	{
+		sum = 0;
+		for(int count2 = 0; count2 < 3; count2++)
+		{
+			sum += gameBoard[count1][count2];
+		}
+		//chosing location
+		if (sum == 2*letter)
+		{
+			row = count1;
+			for(int count2 = 0; count2 < 3; count2++)
+				if(gameBoard[count1][count2] == 0)
+					col = count2;
+		}
+		else
+			count1++;
+	}
+	//check cols
+	count1 = 0;
+	if(sum != 2*letter)
+	{
+		while(sum != 2*letter && count1 < 3)
+		{
+			sum = 0;
+			for(int count2 = 0; count2 < 3; count2++)
+			{
+				sum += gameBoard[count2][count1];
+			}
+			//chosing location
+			if (sum == 2*letter)
+			{
+				col = count1;
+				for(int count2 = 0; count2 < 3; count2++)
+					if(gameBoard[count2][count1] ==0)
+						row = count2;
+			}
+			else
+				count1++;
+		}
+	}
+	//check for left diagoal victory
+	if(sum != 2*letter)
+	{
+		sum = 0;
+		for(int count = 0; count < 3; count++)
+			sum += gameBoard[count][count];
+		//chosing location
+		if (sum == 2*letter)
+			for(int count = 0; count < 3; count++)
+			{
+				if(gameBoard[count][count] == 0)
+				{
+					row = count;
+					col = count;
+				}
+			}
+	}
+	//check for right diagoal victory
+	if(sum != 2*letter)
+	{
+		sum = 0;
+		for(int count3 = 2; count3 >= 0; count3--)
+		{
+			int count2 = 2-count3;
+			sum += gameBoard[count3][count2];
+		}
+		//chosing location
+		if (sum == 2*letter)
+			for(int count3 = 2; count3 >= 0; count3--)
+			{
+				int count2 = 2-count3;
+				if(gameBoard[count3][count2] == 0)
+				{
+					row = count3;
+					col = count2;
+				}
+			}
+	}
+	if (sum == 2*letter)
+		//temp for testing
+		gameBoard[row][col] = 1;
+
+	//check if player can win
+	sum = 0;
+	count1 = 0;
+	row = 0;
+	col = 0;
+
+	//check rows
+	while((sum != 2*-letter) && count1 < 3)
+	{
+		sum = 0;
+		for(int count2 = 0; count2 < 3; count2++)
+		{
+			sum += gameBoard[count1][count2];
+		}
+		//chosing location
+		if (sum == 2*-letter)
+		{
+			row = count1;
+			for(int count2 = 0; count2 < 3; count2++)
+				if(gameBoard[count1][count2] == 0)
+					col = count2;
+		}
+		else
+			count1++;
+	}
+	//check cols
+	count1 = 0;
+	if(sum != 2*-letter)
+	{
+		while(sum != 2*-letter && count1 < 3)
+		{
+			sum = 0;
+			for(int count2 = 0; count2 < 3; count2++)
+			{
+				sum += gameBoard[count2][count1];
+			}
+			//chosing location
+			if (sum == 2*-letter)
+			{
+				col = count1;
+				for(int count2 = 0; count2 < 3; count2++)
+					if(gameBoard[count2][count1] ==0)
+						row = count2;
+			}
+			else
+				count1++;
+		}
+	}
+	//check for left diagoal victory
+	if(sum != 2*-letter)
+	{
+		sum = 0;
+		for(int count = 0; count < 3; count++)
+			sum += gameBoard[count][count];
+		//chosing location
+		if (sum == 2*-letter)
+			for(int count = 0; count < 3; count++)
+			{
+				if(gameBoard[count][count] == 0)
+				{
+					row = count;
+					col = count;
+				}
+			}
+	}
+	//check for right diagoal victory
+	if(sum != 2*-letter)
+	{
+		sum = 0;
+		for(int count3 = 2; count3 >= 0; count3--)
+		{
+			int count2 = 2-count3;
+			sum += gameBoard[count3][count2];
+		}
+		//chosing location
+		if (sum == 2*-letter)
+			for(int count3 = 2; count3 >= 0; count3--)
+			{
+				int count2 = 2-count3;
+				if(gameBoard[count3][count2] == 0)
+				{
+					row = count3;
+					col = count2;
+				}
+			}
+	}
+	if (sum == 2*-letter)
+	//temp for testing
+		gameBoard[row][col] = 1;
+
+	//check if it can make 2 in a row
+
+	//place in most empty area
+}
 
 void PlayerMove(int gameBoard[3][3], int letter)
 {
